e9445f0031f3527af42074b27a3d325f8d7ae988
Brian Chaia <brian@banhaia.com>
2023-12-12 12:18:10 -0300

    chore: google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
@@ -7,7 +9,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <script src="https://www.googletagmanager.com/gtag/js?id=G-XJMZCEWND1"></script>
+
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-XJMZCEWND1');
+
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
