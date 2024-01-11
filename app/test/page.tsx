6a2ab27e72ec89f23f7cf973af16bbb75983c363
Brian Chaia <brian@banhaia.com>
2024-01-11 17:16:45 -0300

    test: agrego form clientify

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>test clientify</h2>
+
+      <script
+        async
+        type="text/javascript"
+        src="https://api.clientify.net/web-marketing/superforms/script/127575.js"
+      ></script>
+    </div>
+  );
+}
